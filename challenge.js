faa6a9e8b675a4e4ca6a77180ab9989161a37310
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2023-01-05 10:06:10 +0100

    first commit

---
+++ challenge.js
@@ -0,0 +1,15 @@
+// challenge.ts
+var prettyPrintWilder = function (users) {
+    // const prettyPrintWilder = (users : User[]) => {
+    users.map(function (user) {
+        console.log("".concat(user.name, " is ").concat(user.age, " years old"));
+    });
+};
+var wilders = [];
+var user1 = { name: "Pierre", age: 23 };
+var user2 = { name: "Paul", age: 32 };
+var user3 = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
